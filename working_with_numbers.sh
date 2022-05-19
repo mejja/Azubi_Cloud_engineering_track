3ae5ce4cd6e4a90457c47baf430b5ec7a6719aea
mejja <majorkiema.mk@gmail.com>
2022-05-19 16:25:48 +0300

    Create working_with_numbers.sh

---
+++ working_with_numbers.sh
@@ -0,0 +1,15 @@
+
+#! /bin.bash
+#https://github.com/mejja/azubi_cloud_engineering_track/new/working_with_numbers.sh
+x=20
+y=10
+
+sum=$(( $x + $y))
+difference=$(( $x - $y))
+product=$(( $x * $y))
+quotient=$(( $x / $y))
+
+echo " Sum is: $sum \n Difference is: $difference \n Product is: $product \n Quotient is: $quotient \n "
+
+
+
