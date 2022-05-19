c2db9c2d18097825b86bee572e57b00ea7093614
mejja <majorkiema.mk@gmail.com>
2022-05-19 16:50:37 +0300

    Update looping_and_skipping.sh

--- looping_and_skipping.sh
+++ looping_and_skipping.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #Shell Script to out all odd numbers between and 100 using stepsize
-#http://github/mejja/azubi_cloud_engineering_track/looping_and_skipping.sh
+#https://github.com/mejja/azubi_cloud_engineering_track/blob/main/looping_and_skipping.sh
 
 
 for i in {1..100..2}; do
