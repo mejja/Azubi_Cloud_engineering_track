976d1d038ebdbba02bfbabda4e4a9fd43f4e0fd5
mejja <majorkiema.mk@gmail.com>
2022-05-25 18:35:01 +0300

    Update cut_2.sh

--- cut_2.sh
+++ cut_2.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #script to read the 2nd and 7th number of a string entered by user
-#https://github.com/mejja/azubi_cloud_engineering_track/new/main
+#https://github.com/mejja/azubi_cloud_engineering_track/blob/main/cut_2.sh
 echo "Kindly Enter the string"
 read str1
 
