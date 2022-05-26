5497c6c837f2d045713d21c8ba6f34438c762065
mejja <majorkiema.mk@gmail.com>
2022-05-26 19:51:23 +0300

    Update middle_text.sh

--- middle_text.sh
+++ middle_text.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#https://github.com/mejja/azubi_cloud_engineering_track/blob/main/middle_text.sh
+#bash script display line ranges in a text file
 
+#specifying the location of my textfile
+File="/home/major/azubi_scripts/textfile.txt"
+
+#output the contents of the text file using cat
+#Then pipe through head and tail for the specify range
+cat $File |head -n 22 | tail -n +12
